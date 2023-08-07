c9408aaa6006b40e152a4637b8f15fd8769b8e4e
Chey <ctenmanikdavid@gmail.com>
2023-08-07 13:05:13 +0000

    Hide secret key via heroku config

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,9 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-280z&tqfp-4=2w-(5_v0^*qhl=x&v5d_2^i_90#5y4r^@_9et0"
-)
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
