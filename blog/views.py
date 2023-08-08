73c6fa6847fe4f49644e4016305b68b43e4ae677
Chey <ctenmanikdavid@gmail.com>
2023-08-08 15:39:24 +0000

    Add comment form in post_detail.html using crispy

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 
 class PostList(generic.ListView):
@@ -22,5 +23,10 @@ class PostDetail(View):
         return render(
             request,
             "post_detail.html",
-            {"post": post, "comments": comments, "liked": liked},
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked,
+                "comment_form": CommentForm(),
+            },
         )
